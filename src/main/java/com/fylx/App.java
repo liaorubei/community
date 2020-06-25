6b5160a2a97261aea22e8417d2b2756496459d48
liaorubei <liaorubei@outlook.com>
2020-06-25 21:01:18 +0800

    -

--- src/main/java/com/fylx/App.java
+++ src/main/java/com/fylx/App.java
@@ -10,6 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -21,11 +24,21 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+/*        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
         String[] names = context.getBeanDefinitionNames();
         for (String name : names) {
             System.out.println(name);
         }
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");*/
+        //中文中文
+        List<User> list = new ArrayList<>();
+        Export(list, User.class);
+    }
+
+
+    public static <T> void Export(List<T> list, Class<T> c) {
+        // c.get
+
+        System.out.println("");
     }
 }
