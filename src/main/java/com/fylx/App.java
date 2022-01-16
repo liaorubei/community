0b591d775c694425dd07b0bf88f551dd77a43209
liaorubei <liaorubei@outlook.com>
2022-01-16 22:19:29 +0800

    -

--- src/main/java/com/fylx/App.java
+++ src/main/java/com/fylx/App.java
@@ -1,31 +1,13 @@
 package com.fylx;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
-import com.fylx.entity.User;
-import com.fylx.mapper.UserMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-/**
- * Hello world!
- */
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
-/*        String[] names = context.getBeanDefinitionNames();
-        for (String name : names) {
-            System.out.println(name);
-        }*/
         System.out.println("Hello World!");
     }
 }
